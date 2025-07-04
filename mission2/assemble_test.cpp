3741058e79ec6aa1b993550b6ff320ea27ba92f8
KangdongHwa <dh.kang8828@gmail.com>
2025-07-04 12:47:11 +0900

    mission2: init and update gitignore

---
+++ mission2/assemble_test.cpp
@@ -0,0 +1,5 @@
+#include "gmock/gmock.h"
+
+TEST(BASIC_TEST, TC1) {
+	EXPECT_EQ(1, 1);
+}
---
+++ mission2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#ifdef _DEBUG
+
+#include "gmock/gmock.h"
+
+int main()
+{
+    testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
+}
+
+#else
+
+int main()
+{
+  
+}
+
+#endif
